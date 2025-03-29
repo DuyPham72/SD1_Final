a773e4cc60c633483ef747845a981e473e57de7e
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-03-28 19:26:31 -0500

    add error handler

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.get('/favicon.png', (req, res) => res.status(204).end());
+
 // Test route
 app.get('/test', async (req, res) => {
   res.json({ message: 'Server is running' });
